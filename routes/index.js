2562457078c9423cda27cb95ea884ceb570d7b76
sanmiranda <sanmiranda@hotmail.com>
2018-12-06 17:32:55 -0600

    holy fuck :hankey: the semicolon

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const router  = express.Router();
-const Course = require('../models/Course')
+const Course = require('../models/Courses')
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
 });
 
 router.get('/api_js', (req, res, next)=>{
-  Course.find(), (err,campos)=>{
- if (err){
-   next(err)
- }else{
-   res.status(200).json({campos:campos})
- }
-  }
+  Course.find()
+  .then(campos => {
+    res.status(200).json(campos)
+  })
+  .catch(err => {
+    throw err
+  })
 })
 
 module.exports = router;

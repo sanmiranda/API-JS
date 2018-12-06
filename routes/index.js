dc2fe6042194950db941f78d5d350782d198e96a
sanmiranda <sanmiranda@hotmail.com>
2018-12-06 17:34:27 -0600

    holy fuck now the 's'

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const Course = require('../models/Courses')
+const Course = require('../models/Course');
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
